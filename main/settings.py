5f19977a41732be5442c7b1ffb522b57a29729b7
Dennis Munyu <ituromunyu@gmail.com>
2020-05-20 08:16:12 +0300

    updated settings

--- main/settings.py
+++ main/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f0fab82d852b5d46aff8c70ab05b5804649d937d82f6950e'
-
+#SECRET_KEY = 'f0fab82d852b5d46aff8c70ab05b5804649d937d82f6950e'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['travel-ke.herokuapp.com']
 
 
 # Application definition
